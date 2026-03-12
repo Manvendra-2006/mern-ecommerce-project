45c96ea62bb7c036e80c1045cd07bfc2cc358222
Manvendra_2006 <manvendrabhardwaj8@gmail.com>
2026-03-12 20:35:54 +0530

    feat: add JWT based authentication in backend

--- Backend/Routes/authRoutes.js
+++ Backend/Routes/authRoutes.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import { signupUser } from '../controllers/authController.js'
+import { loginuser } from '../controllers/authController.js'
 const router = express.Router()
 router.post("/signup",signupUser)
+router.post("/login",loginuser)
 export default router;
--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,7 +1,8 @@
 import User from "../models/User.js";
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 export const signupUser = async (req,resp) =>{
-    const {name,email,password} = req.body
+    const {name,email,password} = req.body // Destructuring
     const userExists = await User.findOne({email})
     if(userExists){
         return resp.status(400).json({message :"User already exists"})
@@ -13,4 +14,41 @@ export const signupUser = async (req,resp) =>{
         password:hashPassword
     })
     resp.json({message:"User registered successfully"})
+}
+export const loginuser = async (req,resp) =>{
+    try{
+        const {email,password} = req.body
+
+        const user = await User.findOne({email})
+
+        if(!user){
+            return resp.status(400).json({message:"User not found"})
+        }
+
+        const match = await bcrypt.compare(password,user.password)
+
+        if(!match){
+           return resp.status(400).json({message:"Invalid Credentials"})
+        }
+
+        const token = jwt.sign(  // Ye method hoti jwt ki token generate krne ki
+            {id:user._id},
+            process.env.JWT_TOKEN,
+            {expiresIn : "7d"}
+        ) 
+
+        resp.json({
+            message:"Login Successfully",
+            token,
+            user:{
+                id:user._id,
+                name:user.name,
+                email:user.email
+            }
+        })
+    }
+    catch(error){
+        console.log("Login Error:",error)
+        resp.status(500).json({message:"Server Error"})
+    }
 }
