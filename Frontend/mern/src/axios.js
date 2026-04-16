c449f769fcf97231512c6b20c9d318b3df9ac161
Manvendra_2006 <manvendrabhardwaj8@gmail.com>
2026-04-16 13:00:23 +0530

    chore : fix axios baseURL for production

--- Frontend/mern/src/axios.js
+++ Frontend/mern/src/axios.js
@@ -1,5 +1,7 @@
-import axios from 'axios'
+import axios from "axios";
+
 const api = axios.create({
-    baseURL:"http://localhost:8900/api"
-})
-export default api
+  baseURL: "/api"
+});
+
+export default api;
